0c7bfd235b0afa2009ae2eb36ad075ac3de73f94
ALEX874681 <TB24004@ues.edu.sv>
2024-10-24 17:10:40 -0600

    se agrego el texbox

--- Clave1_SistemaVeterinaria_Grupo1/FormCita.Designer.cs
+++ Clave1_SistemaVeterinaria_Grupo1/FormCita.Designer.cs
@@ -30,25 +30,38 @@ namespace Clave1_SistemaVeterinaria_Grupo1
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(68, 30);
+            this.label1.Location = new System.Drawing.Point(91, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.Size = new System.Drawing.Size(82, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "ID CLIENTE";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(289, 35);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 21);
+            this.textBox1.TabIndex = 1;
+            // 
             // FormCita
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(618, 343);
+            this.ClientSize = new System.Drawing.Size(661, 305);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "FormCita";
-            this.Text = "FormCita";
+            this.Text = "<";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,5 +70,6 @@ namespace Clave1_SistemaVeterinaria_Grupo1
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
