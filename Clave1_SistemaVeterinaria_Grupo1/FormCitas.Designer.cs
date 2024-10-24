6b9e478afde58942b5924a4db095955f04a4d93f
ALEX874681 <TB24004@ues.edu.sv>
2024-10-24 17:33:57 -0600

    se agregó el formulario cita

---
+++ Clave1_SistemaVeterinaria_Grupo1/FormCitas.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace Clave1_SistemaVeterinaria_Grupo1
+{
+    partial class FormCitas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIDCliente = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtIDMascota = new System.Windows.Forms.TextBox();
+            this.txtIDCita = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID Cliente  ";
+            // 
+            // txtIDCliente
+            // 
+            this.txtIDCliente.Location = new System.Drawing.Point(221, 13);
+            this.txtIDCliente.Name = "txtIDCliente";
+            this.txtIDCliente.Size = new System.Drawing.Size(100, 20);
+            this.txtIDCliente.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ID Mascota  ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ID Cita";
+            // 
+            // txtIDMascota
+            // 
+            this.txtIDMascota.Location = new System.Drawing.Point(221, 64);
+            this.txtIDMascota.Name = "txtIDMascota";
+            this.txtIDMascota.Size = new System.Drawing.Size(100, 20);
+            this.txtIDMascota.TabIndex = 4;
+            // 
+            // txtIDCita
+            // 
+            this.txtIDCita.Location = new System.Drawing.Point(221, 121);
+            this.txtIDCita.Name = "txtIDCita";
+            this.txtIDCita.Size = new System.Drawing.Size(100, 20);
+            this.txtIDCita.TabIndex = 5;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(478, 10);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(389, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Fecha y Hora";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(392, 143);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 34);
+            this.btnAgregar.TabIndex = 8;
+            this.btnAgregar.Text = "Agregar Cita";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(521, 143);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(96, 34);
+            this.btnCancelar.TabIndex = 9;
+            this.btnCancelar.Text = "Cancelar Cita";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // FormCitas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 254);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtIDCita);
+            this.Controls.Add(this.txtIDMascota);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIDCliente);
+            this.Controls.Add(this.label1);
+            this.Name = "FormCitas";
+            this.Text = "FormCitas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIDCliente;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtIDMascota;
+        private System.Windows.Forms.TextBox txtIDCita;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
