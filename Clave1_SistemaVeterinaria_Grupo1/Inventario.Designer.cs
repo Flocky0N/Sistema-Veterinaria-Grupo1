e4fc91244737a95d961bb23c3f85937389371e8c
Yasmi-hub <yasmigonzalez172125@gmail.com>
2024-11-08 15:41:48 -0600

    Incorporación de código al formulario inventario

--- Clave1_SistemaVeterinaria_Grupo1/Inventario.Designer.cs
+++ Clave1_SistemaVeterinaria_Grupo1/Inventario.Designer.cs
@@ -29,11 +29,9 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.txtIdProducto = new System.Windows.Forms.TextBox();
             this.txtNombre = new System.Windows.Forms.TextBox();
             this.txtCantidad = new System.Windows.Forms.TextBox();
             this.txtPrecio = new System.Windows.Forms.TextBox();
@@ -47,25 +45,17 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(187, 9);
+            this.label1.Location = new System.Drawing.Point(208, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(138, 39);
             this.label1.TabIndex = 0;
             this.label1.Text = "Inventario";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(58, 77);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(81, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Id del producto:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(58, 143);
+            this.label3.Location = new System.Drawing.Point(62, 84);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(47, 13);
             this.label3.TabIndex = 2;
@@ -74,7 +64,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(58, 207);
+            this.label4.Location = new System.Drawing.Point(62, 148);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(102, 13);
             this.label4.TabIndex = 3;
@@ -83,37 +73,30 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(58, 280);
+            this.label5.Location = new System.Drawing.Point(62, 221);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(40, 13);
             this.label5.TabIndex = 4;
             this.label5.Text = "Precio:";
             this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
-            // txtIdProducto
-            // 
-            this.txtIdProducto.Location = new System.Drawing.Point(177, 69);
-            this.txtIdProducto.Name = "txtIdProducto";
-            this.txtIdProducto.Size = new System.Drawing.Size(100, 20);
-            this.txtIdProducto.TabIndex = 5;
-            // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(177, 135);
+            this.txtNombre.Location = new System.Drawing.Point(181, 76);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(339, 20);
             this.txtNombre.TabIndex = 6;
             // 
             // txtCantidad
             // 
-            this.txtCantidad.Location = new System.Drawing.Point(177, 199);
+            this.txtCantidad.Location = new System.Drawing.Point(181, 140);
             this.txtCantidad.Name = "txtCantidad";
             this.txtCantidad.Size = new System.Drawing.Size(137, 20);
             this.txtCantidad.TabIndex = 7;
             // 
             // txtPrecio
             // 
-            this.txtPrecio.Location = new System.Drawing.Point(177, 272);
+            this.txtPrecio.Location = new System.Drawing.Point(181, 213);
             this.txtPrecio.Name = "txtPrecio";
             this.txtPrecio.Size = new System.Drawing.Size(137, 20);
             this.txtPrecio.TabIndex = 8;
@@ -121,17 +104,18 @@
             // btnAgregar
             // 
             this.btnAgregar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.btnAgregar.Location = new System.Drawing.Point(47, 340);
+            this.btnAgregar.Location = new System.Drawing.Point(51, 281);
             this.btnAgregar.Name = "btnAgregar";
             this.btnAgregar.Size = new System.Drawing.Size(75, 23);
             this.btnAgregar.TabIndex = 9;
             this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // btnEliminar
             // 
             this.btnEliminar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.btnEliminar.Location = new System.Drawing.Point(177, 340);
+            this.btnEliminar.Location = new System.Drawing.Point(181, 281);
             this.btnEliminar.Name = "btnEliminar";
             this.btnEliminar.Size = new System.Drawing.Size(75, 23);
             this.btnEliminar.TabIndex = 10;
@@ -141,7 +125,7 @@
             // btnModificar
             // 
             this.btnModificar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.btnModificar.Location = new System.Drawing.Point(314, 340);
+            this.btnModificar.Location = new System.Drawing.Point(318, 281);
             this.btnModificar.Name = "btnModificar";
             this.btnModificar.Size = new System.Drawing.Size(75, 23);
             this.btnModificar.TabIndex = 11;
@@ -151,7 +135,7 @@
             // btnConsultar
             // 
             this.btnConsultar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.btnConsultar.Location = new System.Drawing.Point(459, 340);
+            this.btnConsultar.Location = new System.Drawing.Point(463, 281);
             this.btnConsultar.Name = "btnConsultar";
             this.btnConsultar.Size = new System.Drawing.Size(75, 23);
             this.btnConsultar.TabIndex = 12;
@@ -170,11 +154,9 @@
             this.Controls.Add(this.txtPrecio);
             this.Controls.Add(this.txtCantidad);
             this.Controls.Add(this.txtNombre);
-            this.Controls.Add(this.txtIdProducto);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Inventario";
             this.Text = "Inventario";
@@ -186,11 +168,9 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox txtIdProducto;
         private System.Windows.Forms.TextBox txtNombre;
         private System.Windows.Forms.TextBox txtCantidad;
         private System.Windows.Forms.TextBox txtPrecio;
--- Clave1_SistemaVeterinaria_Grupo1/Inventario.cs
+++ Clave1_SistemaVeterinaria_Grupo1/Inventario.cs
@@ -7,18 +7,114 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using Clave1_SistemaVeterinaria_Grupo1.Clases;
+
 
 namespace Clave1_SistemaVeterinaria_Grupo1
 {
-    public partial class Inventario: Form
+    public partial class Inventario : Form
     {
+        private conexionGrupo1 conexionDB;
+      
+
         public Inventario()
         {
             InitializeComponent();
+            conexionDB = new conexionGrupo1();
         }
+
+      
 
         private void label5_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (!validarCampos())
+            {
+                MessageBox.Show("Ingrese datos en los campos correspondientes: " + Camposvacios());
+                return;
+            }
+            Inventary inventario = new Inventary (txtNombre.Text, txtCantidad.Text, txtPrecio.Text);
+            ingresaProducto(inventario);
+
+        }
+
+        public void ingresaProducto (Inventary i)
+        {
+            MySqlConnection conexion = conexionDB.AbrirConexion();
+            MySqlCommand consulta = new MySqlCommand();
+
+            consulta.Connection = conexion;
+            consulta.CommandText = "INSERT INTO producto(nombre,cantidad, precio) " +
+                "VALUES(@nombre, @cantidad, @precio)";
+
+            consulta.Parameters.AddWithValue("@nombre", i.Nombre);
+            consulta.Parameters.AddWithValue("@cantidad", i.Cantidad);
+            consulta.Parameters.AddWithValue("@precio", i.Precio);
+
+            try
+            {
+                int rowsAffected = consulta.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("El producto se ha agregado exitosamente");
+                }
+                else
+                {
+                    MessageBox.Show("No se pudo agregar el producto");
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Eror al procesar la instrucción" + ex);
+            }
+            finally
+            {
+                conexionDB.CerrarConexion();
+            }
+        }
+        public bool validarCampos()
+        {
+            foreach (Control item in this.Controls)
+            {
+                if (item is TextBox)
+                {
+                    if (item.Name != "textId")
+                    {
+                        if (TextoVacio(item.Text))
+                            return false;
+                    }
+                }
+            }
+            return true;
+        }
+        public string Camposvacios()
+        {
+            string campos = "";
+            foreach (Control item in this.Controls)
+            {
+                if (item is TextBox && item.Name != "textId")
+                {
+                    if (TextoVacio(item.Text))
+                        campos += "\n" + item.Name;
+                }
+            }
+            return campos;
+        }
+
+        public bool TextoVacio(string texto)
+        {
+            return string.IsNullOrWhiteSpace(texto);
 
         }
     }
